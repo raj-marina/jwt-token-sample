8a00513a9b2729da7ee0debe0aabacf0686009f2
raj-marina <administrator@macenoah.local>
2016-04-07 17:55:05 +0530

    aq

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,10 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-
-    pkg: grunt.file.readJSON('package.json')
-
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+    files: ['*.js', '*.css'],
+    tasks: ['concat', 'uglify', 'cssmin']
+    }
+  });
 };
